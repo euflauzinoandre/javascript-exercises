54c26aa5e4a4de79569c322c67f26eb20af0ad2c
André Euflauzino <78576825+euflauzinoandre@users.noreply.github.com>
2026-05-26 01:50:11 +0100

    Exercise 7: Shuffle an array

--- foundations/my_exercises/array.js
+++ foundations/my_exercises/array.js
@@ -60,5 +60,15 @@ let sorted = copySorted(arr);
 */
 
 //	Shuffle an array
+function shuffle(arr) {
+	return arr.sort(() => Math.random() - 0.5);
+}
+
+/*
+let arr = [1, 2, 3];
+shuffle(arr); // arr = [3, 2, 1]
+shuffle(arr); // arr = [2, 1, 3]
+shuffle(arr); // arr = [3, 1, 2]
+*/
 
 //	Filter unique array members
