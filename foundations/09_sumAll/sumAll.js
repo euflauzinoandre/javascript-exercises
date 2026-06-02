82405ff56c5c16ccd820668d631ff96167c59ae7
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-06-02 17:19:15 +0100

    foundations/09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+	let result = 0;
+	if ((Number.isNaN(num1)) || Number.isNaN(num2) || !Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0)
+		return "ERROR";
+	if (num1 < num2) {
+		for (let i = num1; i <= num2; i++) {
+			result += i;
+		}
+	}
+	else if (num1 > num2) {
+                for (let i = num1; i >= num2; i--) {
+                        result += i;
+                }
+	}
+	return result;
 };
 
 // Do not edit below this line
