982a0575b9d97e8ac5cc51d340711a0f7734c129
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-07-29 02:36:59 +0100

    create getAverageAge exercise

---
+++ foundations/my_exercises/getAverageAge.js
@@ -0,0 +1,11 @@
+function getAverageAge(arr) {
+	return arr.reduce((sum, current) => sum + current.age, 0) / arr.length;
+}
+
+let john = { name: "John", age: 25 };
+let pete = { name: "Pete", age: 30 };
+let mary = { name: "Mary", age: 29 };
+
+let arr = [john, pete, mary];
+
+console.log(getAverageAge(arr)); // (25 + 30 + 29) / 3 = 28
