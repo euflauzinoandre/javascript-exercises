495c98ce4c2032c782ca17431f571e4fa819fb43
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-08-03 01:41:03 +0100

    Complete Foundations/13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+	const alpha = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+	const cleanedString = string
+		.toLowerCase()
+		.split("")
+		.filter((char) => alpha.includes(char))
+		.join("");
+
+	const reversedString = cleanedString.split("").reverse().join("");
+
+	return cleanedString === reversedString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
