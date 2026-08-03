fe9dc9fc25d805dec200b3835be8579156ae86da
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-08-03 01:57:07 +0100

    Complete Foundations/14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,9 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+	if (n < 0) return "OOPS";
+	if (!Number.isFinite(n) && n !== "0") return 1;
+	if (!Number.isFinite(n) && n === "0") return 0;
+	if (n < 2) return n;
+	else return fibonacci(n - 2) + fibonacci(n - 1);
 };
 
 // Do not edit below this line
