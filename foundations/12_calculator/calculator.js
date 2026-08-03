72f3465814567b10ce353c754340e799c197390c
euflauzinoandre <78576825+euflauzinoandre@users.noreply.github.com>
2026-08-03 01:13:06 +0100

    Complete Foundations/12_calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,33 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr) {
+	return arr.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+	let mult = 1;
+	for (let i = 0; i < arr.length; i++) {
+		mult = mult * arr[i];
+	}
+	return mult;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function (x) {
+	if (x === 0 || x === 1) return 1;
+	return x * factorial(x - 1);
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+	add,
+	subtract,
+	sum,
+	multiply,
+	power,
+	factorial,
 };
